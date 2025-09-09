61a2e95d5054f9e3df515777b25586e536114d59
Elisiei Yehorov <yehorovye@disroot.org>
2025-09-09 19:07:04 +0200

    remove main.go density

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"context"
 	"database/sql"
+	"flag"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/disgoorg/disgo/bot"
 	dgobot "github.com/disgoorg/disgo/bot"
 	dgocache "github.com/disgoorg/disgo/cache"
+	"github.com/disgoorg/snowflake/v2"
 
 	"github.com/disgoorg/disgo"
 	"github.com/disgoorg/disgo/gateway"
@@ -28,46 +31,79 @@ import (
 //go:embed db/sql/schema.sql
 var ddl string
 
+var (
+	globalCmds bool
+	dbPath     string
+)
+
 func init() {
 	if err := env.Load(""); err != nil {
-		logger.Log.Fatalf("%v", err)
+		logger.Log.Fatalf("failed loading environment: %v", err)
 	}
+
+	flag.BoolVar(&globalCmds, "g", true, "upload global commands to discord")
+	flag.StringVar(&dbPath, "db", "database.db", "path to the SQLite database file")
+	flag.Parse()
 }
 
 func main() {
-	dbCtx := context.Background()
+	ctx := context.Background()
 
-	logger.Log.Debug("starting client...")
 	token := os.Getenv("DISCORD_TOKEN")
 	if token == "" {
-		logger.Log.Fatal("missing DISCORD_TOKEN variable")
+		logger.Log.Fatal("missing DISCORD_TOKEN environment variable")
 	}
 
 	lfm := lastfm.New()
-	dbConn, err := sql.Open("sqlite3", "database.db")
-	if err != nil {
-		logger.Log.Fatalf("failed opening database: %v", err)
-	}
-	defer dbConn.Close()
 
-	if _, err := dbConn.ExecContext(dbCtx, ddl); err != nil {
-		logger.Log.Fatalf("failed executing schema: %v", err)
-	}
+	closeConnection, database := initDatabase(ctx, dbPath)
+	defer closeConnection()
 
-	if _, err := db.Prepare(dbCtx, dbConn); err != nil {
-		logger.Log.Fatalf("failed preparing queries: %v", err)
-	}
-
-	database := db.New(dbConn)
-	ctx := cmd.CommandContext{
+	cmdCtx := cmd.CommandContext{
 		LastFM:   lfm,
 		Database: database,
 	}
-	commands.InitDependencies(ctx)
+	commands.InitDependencies(cmdCtx)
 
+	client := initDiscordClient(token)
+	defer client.Close(context.TODO())
+
+	if err := client.OpenGateway(context.TODO()); err != nil {
+		logger.Log.Fatalf("failed to open gateway: %v", err)
+	}
+
+	if globalCmds {
+		uploadGlobalCommands(client)
+	} else {
+		uploadGuildCommands(client)
+	}
+
+	waitForShutdown()
+}
+
+func initDatabase(ctx context.Context, path string) (func() error, *db.Queries) {
+	dbConn, err := sql.Open("sqlite3", path)
+	if err != nil {
+		logger.Log.Fatalf("failed opening database: %v", err)
+	}
+
+	if _, err := dbConn.ExecContext(ctx, ddl); err != nil {
+		logger.Log.Fatalf("failed executing schema: %v", err)
+	}
+
+	_, err = db.Prepare(ctx, dbConn)
+	if err != nil {
+		logger.Log.Fatalf("failed preparing queries: %v", err)
+	}
+
+	return dbConn.Close, db.New(dbConn)
+}
+
+func initDiscordClient(token string) bot.Client {
 	cacheOptions := dgobot.WithCacheConfigOpts(
 		dgocache.WithCaches(dgocache.FlagMembers),
 	)
+
 	options := dgobot.WithGatewayConfigOpts(
 		gateway.WithIntents(
 			gateway.IntentsNonPrivileged,
@@ -79,26 +115,36 @@ func main() {
 	client, err := disgo.New(
 		token,
 		options,
-		dgobot.WithEventListeners(
-			commands.Handler(),
-		),
+		dgobot.WithEventListeners(commands.Handler()),
 		cacheOptions,
 	)
 	if err != nil {
-		logger.Log.Fatalf("failed to instantiate client: %v", err)
-	}
-	defer client.Close(context.TODO())
-
-	if err = client.OpenGateway(context.TODO()); err != nil {
-		logger.Log.Fatalf("failed to open gateway: %v", err)
+		logger.Log.Fatalf("failed to instantiate Discord client: %v", err)
 	}
 
-	if _, err = client.Rest().SetGlobalCommands(client.ApplicationID(), commands.All()); err != nil {
-		logger.Log.Fatalf("failed registering commands: %v", err)
-	}
-	logger.Log.Info("registered slash commands")
+	return client
+}
 
+func uploadGlobalCommands(client bot.Client) {
+	_, err := client.Rest().SetGlobalCommands(client.ApplicationID(), commands.All())
+	if err != nil {
+		logger.Log.Fatalf("failed registering global commands: %v", err)
+	}
+	logger.Log.Info("registered global slash commands")
+}
+
+func uploadGuildCommands(client bot.Client) {
+	guildId := snowflake.GetEnv("GUILD_ID")
+	_, err := client.Rest().SetGuildCommands(client.ApplicationID(), guildId, commands.All())
+	if err != nil {
+		logger.Log.Fatalf("failed registering global commands: %v", err)
+	}
+	logger.Log.Infof("registered guild slash commands to guild '%s'", guildId.String())
+}
+
+func waitForShutdown() {
 	s := make(chan os.Signal, 1)
 	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
 	<-s
+	logger.Log.Info("goodbye :)")
 }
