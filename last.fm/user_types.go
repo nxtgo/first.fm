01ba934501f8efdebb1af04ad73cac7a60fb0f6e
Elisiei Yehorov <yehorovye@disroot.org>
2025-09-21 19:17:48 +0200

    move recenttrack type

--- last.fm/user_types.go
+++ last.fm/user_types.go
@@ -31,6 +31,79 @@ type Timestamp struct {
 	Text     string `xml:",chardata"`
 }
 
+// RecentTrack represents a Last.fm track
+type RecentTrack struct {
+	Artist     RecentTrackArtist `xml:"artist"`
+	Name       string            `xml:"name"`
+	Streamable string            `xml:"streamable"`
+	MBID       string            `xml:"mbid"`
+	Album      RecentTrackAlbum  `xml:"album"`
+	URL        string            `xml:"url"`
+	Images     []Image           `xml:"image"`
+	Date       *RecentTrackDate  `xml:"date"`
+	NowPlaying string            `xml:"nowplaying,attr"`
+}
+
+// RecentTrackArtist represents an artist in a track
+type RecentTrackArtist struct {
+	MBID string `xml:"mbid,attr"`
+	Name string `xml:",chardata"`
+}
+
+// RecentTrackAlbum represents an album in a track
+type RecentTrackAlbum struct {
+	MBID string `xml:"mbid,attr"`
+	Name string `xml:",chardata"`
+}
+
+// RecentTrackDate represents a track's play date
+type RecentTrackDate struct {
+	UTS  string `xml:"uts,attr"`
+	Text string `xml:",chardata"`
+}
+
+// IsNowPlaying returns true if the track is currently playing
+func (t *RecentTrack) IsNowPlaying() bool {
+	return t.NowPlaying == "true"
+}
+
+// GetPlayTime returns the track's play time as a Go time.Time
+func (t *RecentTrack) GetPlayTime() (time.Time, error) {
+	if t.Date == nil || t.Date.UTS == "" {
+		return time.Time{}, nil
+	}
+
+	unixTime, err := strconv.ParseInt(t.Date.UTS, 10, 64)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return time.Unix(unixTime, 0), nil
+}
+
+// GetLargestImage returns the largest available image for the track
+func (t *RecentTrack) GetLargestImage() *Image {
+	sizes := []string{"mega", "extralarge", "large", "medium", "small"}
+	for _, size := range sizes {
+		for _, img := range t.Images {
+			if img.Size == size && img.URL != "" {
+				return &img
+			}
+		}
+	}
+	return nil
+}
+
+// GetImageBySize returns an image of the specified size for the track
+func (t *RecentTrack) GetImageBySize(size string) *Image {
+	for _, img := range t.Images {
+		if img.Size == size && img.URL != "" {
+			return &img
+		}
+	}
+	return nil
+}
+
 // RecentTracks represents the recent tracks response
 type RecentTracks struct {
 	User       string  `xml:"user,attr"`
